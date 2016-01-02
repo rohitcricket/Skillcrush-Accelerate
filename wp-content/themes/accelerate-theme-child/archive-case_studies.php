29ce070e5764cd2521212687e4b650715f6e6cdb
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro.local>
2016-01-01 20:15:44 -0800

    redid archive-case_studies with page.php file

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the archive of case studies.
+ * The template for displaying archive pages
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -8,60 +8,43 @@
  * different template.
  *
  * @package WordPress
- * @subpackage Accelerate Marketing
- * @since Accelerate Marketing 1.0
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
  */
 
 get_header(); ?>
 
-<section class="home-page">
-	<div class="site-content">
-		<?php while ( have_posts() ) : the_post(); 
-			
-
-			$image_1 = get_field('image_1');
-			$size = "medium";
-			$services = get_field('services');
-		?>
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field("image_1");
+				$size = "medium";
+				$services = get_field("services");
+			?>
 
 			<article class = "case-study">
 		<aside class = "case-study-sidebar">
 
-			
-			<h2><?php the_title(); ?></h2>
+			<h2><a href = "<$php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<h5><?php echo $services; ?></h5>
 
 					<?php the_excerpt(); ?>
 
-					<p><a href = "<$php the_permalink(); ?>">Site Link</a></p>
+					<p><strong><a href = "<$php the_permalink(); ?>">View Project</a></strong></p>
 				</aside>
 
 				<div class="case-study-images">
-					<?php if($image_1) { ?>
-						
+					<?php if($image_1) { 
 						echo wp_get_attachment_image( $image_1, $size );
-					<?php } ?>
-					<?php if($image_2) { ?>
-				
-						echo wp_get_attachment_image( $image_2, $size );
-					<?php } ?>
-					<?php if($image_3) { ?>
-						
-						echo wp_get_attachment_image( $image_3, $size );
-					<?php } ?>
+					} ?>
 
 				</div>
 			</article>
 
-			<?php the_excerpt(); ?>
+			<?php endwhile; // end of the loop. ?>
 
-			<div class='homepage-hero'>
-				<?php the_content(); ?>
-				<p>Is this working?</p>
-				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
-			</div>
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
-</section><!-- .home-page -->
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
 
 <?php get_footer(); ?>
