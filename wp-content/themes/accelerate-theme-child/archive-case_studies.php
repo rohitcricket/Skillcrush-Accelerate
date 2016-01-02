0bf0e6c0288416c7b653d508e1077f33926e6e67
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro.local>
2016-01-01 20:38:21 -0800

    fixed  to ?php

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -25,19 +25,20 @@ get_header(); ?>
 			<article class = "case-study">
 		<aside class = "case-study-sidebar">
 
-			<h2><a href = "<$php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<h2><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<h5><?php echo $services; ?></h5>
 
 					<?php the_excerpt(); ?>
 
-					<p><strong><a href = "<$php the_permalink(); ?>">View Project</a></strong></p>
+					<p><strong><a href = "<?php the_permalink(); ?>">View Project</a></strong></p>
 				</aside>
 
 				<div class="case-study-images">
+					<a href = "<?php the_permalink(); ?>">
 					<?php if($image_1) { 
 						echo wp_get_attachment_image( $image_1, $size );
 					} ?>
-
+					</a>
 				</div>
 			</article>
 
